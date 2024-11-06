ac0c1bb7bc3338ff580a56c3c00ada6163677e91
Charles <charleszecevic@gmail.com>
2024-11-06 16:29:59 +0100

    add nav

--- beebs-app/src/components/organisms/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircularProgress } from "@mui/material";
-
-export const Loading = () => {
-  return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <CircularProgress color="inherit" />
-    </div>
-  );
-};
--- beebs-app/src/utils/dateHelpers.ts
+++
@@ -1,6 +0,0 @@
-import { format, parseISO } from "date-fns";
-import { fr } from "date-fns/locale";
-
-export const formatDate = (date: string) => {
-  return format(parseISO(date), "dd/MM/yyyy", { locale: fr });
-};
